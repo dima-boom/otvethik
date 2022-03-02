2d860fe04c3441b04770b4de0561517b6731d392
dima-boom <78297357+dima-boom@users.noreply.github.com>
2022-03-02 16:17:48 +0300

    Update Musor.py

--- Musor.py
+++ Musor.py
@@ -3,19 +3,20 @@ try:
 	from vk_api.utils import get_random_id
 	from vk_api.longpoll import VkLongPoll, VkEventType
 
-    con = psycopg2.connect(
-      database="d4c41h4i7du8mq", 
-      user="svxwcgxnatvdtk", 
-      password="1a8a2996f756536aaf66bdbc9a1ff9f67d97961e6007fc0c652117796470e4ca", 
-      host="ec2-52-208-145-55.eu-west-1.compute.amazonaws.com", 
-      port="5432"
-    )
+	con = psycopg2.connect(
+	database="d4c41h4i7du8mq", 
+	user="svxwcgxnatvdtk", 
+	password="1a8a2996f756536aaf66bdbc9a1ff9f67d97961e6007fc0c652117796470e4ca", 
+	host="ec2-52-208-145-55.eu-west-1.compute.amazonaws.com", 
+	port="5432"
+	)
+	cur = con.cursor()
 
 	# 671ec570b10a110c23e3f1958364140e97764acb8f0cd75b74a1973085c58d6ee4575e98cec0e4d78d23b
 
 	cur.execute(f"SELECT * FROM tab WHERE id = '5073415776'")
-        vvvb = cur.fetchall()
-        token = vvvb[0][2]
+	vvvb = cur.fetchall()
+	token = vvvb[0][2]
 
 	token = str(token)
 	authorize = vk_api.VkApi(token=token)
@@ -23,26 +24,26 @@ try:
 	admin = 574170405
 	for event in longpoll.listen():
 	    if event.type == VkEventType.MESSAGE_NEW and event.text:
-	        reseived_message = event.text.lower()
-	        sender = event.peer_id
-	        if event.to_me:
-	        	print(sender)
-	        	try:
-	        		authorize.get_api().messages.send(peer_id=sender, message='250₽ - 1000 Подписчиков.\nhttps://vk.com/write-210750746', random_id=0)
+		reseived_message = event.text.lower()
+		sender = event.peer_id
+		if event.to_me:
+			print(sender)
+			try:
+				authorize.get_api().messages.send(peer_id=sender, message='250₽ - 1000 Подписчиков.\nhttps://vk.com/write-210750746', random_id=0)
 
-	        	except vk_api.Captcha:
-	                cycle = True
-	                while cycle:
-	                    try:
-	                        authorize.get_api().messages.send(peer_id=sender, message='250₽ - 1000 Подписчиков.\nhttps://vk.com/write-210750746', random_id=0)
-	                    except vk_api.Captcha as cptch:
-	                        result_solve_captcha = vc.solve(sid=int(cptch.sid), s=1)
-	                        try:
-	                            cptch.try_again(result_solve_captcha)
-	                            cycle = False
-	                        except vk_api.Captcha as cptch2:
-	                            pass
-	                    except:
-	                        pass
+			except vk_api.Captcha:
+			cycle = True
+			while cycle:
+			    try:
+				authorize.get_api().messages.send(peer_id=sender, message='250₽ - 1000 Подписчиков.\nhttps://vk.com/write-210750746', random_id=0)
+			    except vk_api.Captcha as cptch:
+				result_solve_captcha = vc.solve(sid=int(cptch.sid), s=1)
+				try:
+				    cptch.try_again(result_solve_captcha)
+				    cycle = False
+				except vk_api.Captcha as cptch2:
+				    pass
+			    except:
+				pass
 except:
 	os.system('python Musor.py')
